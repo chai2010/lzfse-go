3382458895b8af25e2a434a1b847d3a71cc57c70
chai2010 <chaishushan@gmail.com>
2016-07-14 19:25:05 +0800

    zz

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # lzfse for Go
 
+## LZFSE
+
+LZFSE is a Lempel-Ziv style data compression algorithm using Finite State Entropy coding.
+It targets similar compression rates at higher compression and decompression speed compared to deflate using zlib.
+It is introduced in the
+[Compression library](https://developer.apple.com/library/mac/documentation/Performance/Reference/Compression/index.html) with OS X 10.11 and iOS 9.
+
+See https://github.com/lzfse/lzfse
+
 TODO
---
+++ doc.go
@@ -0,0 +1,20 @@
+// Copyright 2016 <chaishushan{AT}gmail.com>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+Package lzfse implements a decoder and encoder for LZFSE Compression library.
+
+LZFSE is defined at:
+https://github.com/lzfse/lzfse
+
+Install
+
+Install `GCC` or `MinGW` (http://tdm-gcc.tdragon.net/download) at first, and
+then run these commands:
+
+    1. `go get github.com/chai2010/lzfse-go`
+    2. `go run hello.go`
+
+*/
+package lzfse
